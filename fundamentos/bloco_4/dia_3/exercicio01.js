cde8fe98c1145610bf316e528fca86c56bd41ab7
Vanessa <vanessamrios@gmail.com>
2021-05-30 15:32:12 -0300

    resolve o exercício 1

---
+++ fundamentos/bloco_4/dia_3/exercicio01.js
@@ -0,0 +1,12 @@
+let n = 5;
+let symbol = "*";
+let linha = "";
+
+for(let index = 1; index <= n; index+=1){
+    linha = linha + symbol;
+}
+
+for(let index = 1; index <= n; index+=1){
+    console.log(linha);
+}
+
