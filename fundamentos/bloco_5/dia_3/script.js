f2d44f4d19bcbb0700039065062d06ae73b3f657
Vanessa <vanessamrios@gmail.com>
2021-06-08 11:10:47 -0300

    resolve o exercício 1

---
+++ fundamentos/bloco_5/dia_3/script.js
@@ -0,0 +1,41 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+
+  function createDaysOfTheMonth () {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const monthDays = document.querySelector("#days");
+
+    for(let day of dezDaysList) {
+      const dayOfMonth = document.createElement("li");
+      dayOfMonth.innerHTML = day;
+
+      monthDays.appendChild(dayOfMonth);
+
+      dayOfMonth.classList.add("day")
+
+      if(day === 24 || day === 25 || day === 31){
+        dayOfMonth.classList.add("holiday");
+      }
+
+      if(day === 4 || day === 11 || day === 18 || day === 31){
+        dayOfMonth.classList.add("fryday");
+      }
+    }
+  }
+
+  createDaysOfTheMonth();
+
+  
+  // Escreva seu código abaixo.
